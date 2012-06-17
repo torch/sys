3e9e7da0d4f4f41d4e45952e5a1923cc5b02795b
Clement Farabet <clement.farabet@gmail.com>
2012-06-17 15:08:33 -0400

    Fixed sys.split()

--- init.lua
+++ init.lua
@@ -133,10 +133,10 @@ function split(str, pat)
    local t = {}  -- NOTE: use {n = 0} in Lua-5.0
    local fpat = "(.-)" .. pat
    local last_end = 1
-   local s, e, cap = str:find(fpat, 1)
+   local s, e, cap = str:find(fpat, last_end)
    while s do
       if s ~= 1 or cap ~= "" then
-         table.insert(t,cap)
+         _G.table.insert(t,cap)
       end
       last_end = e+1
       s, e, cap = str:find(fpat, last_end)
